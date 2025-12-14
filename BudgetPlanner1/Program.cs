4cf17060d99ee080f6083875dec74ef2835f201b
RIC LEE <chuperic@gmail.com>
2025-12-14 22:12:39 +0800

    custom identity logic

--- BudgetPlanner1/Program.cs
+++ BudgetPlanner1/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<BudgetPlanner1User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<BudgetPlanner1Context>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
